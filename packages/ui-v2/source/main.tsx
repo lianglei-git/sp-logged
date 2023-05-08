c2b0b0d3a306936dd245215a445ed461f282d80e
梁磊 <lei.liang@yizhun-ai.com>
2023-05-08 16:21:57 +0800

    feat: three art

--- packages/ui-v2/source/main.tsx
+++ packages/ui-v2/source/main.tsx
@@ -5,14 +5,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { AppStoreContext, appStore } from "./store";
-import Light from "@sp-laboratory/light/dev-lib/index";
+// import Light from "@sp-laboratory/light/dev-lib/index";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AppStoreContext.Provider value={appStore}>
-      <Light version="v1">
-        <App />
-      </Light>
+      {/* <Light version="v1"> */}
+      <App />
+      {/* </Light> */}
     </AppStoreContext.Provider>
   </React.StrictMode>
 );
